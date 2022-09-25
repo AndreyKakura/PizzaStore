c3d51b17ad41c11cdf0bfe196b3c2491a3a47596
Andrey <andreykakura@gmail.com>
2022-09-25 19:22:06 +0300

    role fix

--- src/main/java/com/kakura/pizzastore/model/Role.java
+++ src/main/java/com/kakura/pizzastore/model/Role.java
@@ -1,5 +1,16 @@
 package com.kakura.pizzastore.model;
 
 public enum Role {
-    ROLE_ADMIN, ROLE_USER
+    ROLE_ADMIN("Admin"),
+    ROLE_USER("User");
+
+    private final String roleValue;
+
+    private Role(String roleValue) {
+        this.roleValue = roleValue;
+    }
+
+    public String getRoleValue() {
+        return roleValue;
+    }
 }
