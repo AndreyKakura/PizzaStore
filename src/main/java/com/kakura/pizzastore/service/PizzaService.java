34cf741f3fb3a3d0f4bb9c1181c3b97b892c8239
Andrey <andreykakura@gmail.com>
2022-09-26 21:09:09 +0300

    logging

--- src/main/java/com/kakura/pizzastore/service/PizzaService.java
+++ src/main/java/com/kakura/pizzastore/service/PizzaService.java
@@ -4,6 +4,8 @@ import com.kakura.pizzastore.model.Image;
 import com.kakura.pizzastore.model.Pizza;
 import com.kakura.pizzastore.repository.ImageRepository;
 import com.kakura.pizzastore.repository.PizzaRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +15,9 @@ import java.util.List;
 
 @Service
 public class PizzaService {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private final PizzaRepository pizzaRepository;
     private final ImageRepository imageRepository;
 
@@ -31,45 +36,38 @@ public class PizzaService {
     }
 
     public void save(Pizza pizza, MultipartFile imageFile) {
-
-
         if (imageFile.getSize() != 0) {
-            try {
-                Image image = toImageEntity(imageFile);
-                pizza.setImage(image);
-            } catch (IOException e) {
-                e.printStackTrace();//TODO log
-            }
+            Image image = toImageEntity(imageFile);
+            pizza.setImage(image);
         }
         pizzaRepository.save(pizza);
     }
-
 
     public void update(Long id, Pizza updatedPizza, MultipartFile imageFile) {
         Pizza pizzaToBeUpdated = pizzaRepository.findById(id).get();
         updatedPizza.setId(id);
         if (imageFile.getSize() != 0) {
-            try {
-                Image image = toImageEntity(imageFile);
-                if (pizzaToBeUpdated.getImage() != null) {
-                    image.setId(pizzaToBeUpdated.getImage().getId());
-                }
-                imageRepository.save(image);
-                updatedPizza.setImage(image);
-            } catch (IOException e) {
-                e.printStackTrace();//TODO log
+            Image image = toImageEntity(imageFile);
+            if (pizzaToBeUpdated.getImage() != null) {
+                image.setId(pizzaToBeUpdated.getImage().getId());
             }
+            imageRepository.save(image);
+            updatedPizza.setImage(image);
         }
         pizzaRepository.save(updatedPizza);
     }
 
-    private Image toImageEntity(MultipartFile file) throws IOException {
+    private Image toImageEntity(MultipartFile file) {
         Image image = new Image();
         image.setName(file.getName());
         image.setOriginalFileName(file.getOriginalFilename());
         image.setContentType(file.getContentType());
         image.setSize(file.getSize());
-        image.setBytes(file.getBytes());
+        try {
+            image.setBytes(file.getBytes());
+        } catch (IOException e) {
+            LOGGER.error("Error has occurred while converting file to byte array", e);
+        }
         return image;
     }
 }
